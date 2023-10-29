6ee7bd8f8a1e518003a9d4f385d03cd2f5c9344e
marcelkb <60894942+marcelkb@users.noreply.github.com>
2023-10-29 18:33:24 +0100

    bugfixing enternal sleep was possible in status none case logged sometimes bytes instead of str added func name to better recognize from where errors originate

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,13 +7,13 @@ from utils.sleeping import sleep
 def retry(func):
     async def wrapper(*args, **kwargs):
         retries = 0
-        while retries < RETRY_COUNT:
+        while retries <= RETRY_COUNT:
             try:
                 result = await func(*args, **kwargs)
                 return result
             except Exception as e:
-                logger.error(f"Error | {e}")
-                await sleep(10, 60)
+                logger.error(f"Error | {e} | calling {func.__name__}")
+                await sleep(10, 20)
                 retries += 1
 
     return wrapper
